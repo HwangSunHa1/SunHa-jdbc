d7eaa6df452c109fc06846f1ee42e68e20b4b0b8
HwangSunHa1 <sunha0822@naver.com>
2019-03-22 10:38:44 +0900

    real

---
+++ src/main/java/com/sunha/jdbc/raw/ArticleDao.java
@@ -0,0 +1,11 @@
+package com.sunha.jdbc.raw;
+
+import java.util.List;
+
+public interface ArticleDao {
+	List<Article> listArticles();
+	Article getArticle(String articleId);
+	void addArticle(Article article);
+	void updateArticle(Article article);
+	void deleteArticle(String articleId);
+}
